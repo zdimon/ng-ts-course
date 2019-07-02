e9366f35934922997591bb7afdb777a2cd4603d9
KevinShindel <kevin.shindel@yahoo.com>
2019-07-02 10:56:24 +0300

    home_work2 angular

---
+++ homeassingment/home_work2_ng/user-ng/src/app/app.component.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+
+class User {
+  id: number;
+  username: string;
+  email: string;
+  age: number;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  users: User[] = [{
+    id: 0,
+    username: 'Admin',
+    email: 'admin@gmail.com',
+    age: 30
+  },
+    {
+      id: 1,
+      username: 'Kevin',
+      email: 'shindel@bk.ru',
+      age: 33
+    },
+    {
+      id: 2,
+      username: 'Dima',
+      email: 'zdimon77@gmail.com',
+      age: 40
+    },
+    {
+      id: 3,
+      username: 'Username',
+      email: 'some@email.com',
+      age: 99
+    }];
+
+  username: string;
+  email: string;
+  age: number;
+  searchInput: string;
+
+
+  delete(userId: number) {
+    for (const user of this.users) {
+      if (userId === user.id) {
+        const idx = this.users.indexOf(user);
+        this.users.splice(idx, 1);
+      }
+    }
+  }
+
+  add() {
+    const ID: number = this.users.length;
+    this.users.push({ id: ID, username: this.username, email: this.email, age: this.age });
+  }
+
+  // TODO: Как реализовать поиск?
+  search(emailSearch: string) {
+    for (const user of this.users) {
+      const ID: number = this.users.length;
+      if (emailSearch === user.email) {
+            // DO SOMETHING
+            alert(`User found! ${user.username}`);
+      }
+    }
+  }
+  // TODO: Как реализовать очистку поиска?
+  clear() {
+    return true;
+  }
+}
