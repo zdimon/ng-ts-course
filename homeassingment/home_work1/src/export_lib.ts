3017cec00a00af533f95e50596946659b114dd9f
KevinShindel <kevin.shindel@yahoo.com>
2019-07-08 12:04:01 +0300

    home work 1 done with jQuery

--- homeassingment/home_work1/src/export_lib.ts
+++ homeassingment/home_work1/src/export_lib.ts
@@ -1,3 +1,24 @@
-export function some(){
-  console.log('some text')
-}
+export function operation(e) {
+      let leftOp = Number($('#leftOp').val());
+      let rightOp = Number($('#rightOp').val());
+      let operation = $(e.target).text();
+
+      switch (operation) {
+        case '-':
+          $('#result').val(leftOp - rightOp)
+          break;
+
+        case '+':
+          $('#result').val(leftOp + rightOp)
+          break;
+
+        case '/':
+        $('#result').val(leftOp / rightOp)
+          break;
+          
+        case '*':
+        $('#result').val(leftOp * rightOp)
+          break;
+      }
+
+    };
--- homeassingment/home_work1/src/main.ts
+++ homeassingment/home_work1/src/main.ts
@@ -2,30 +2,7 @@ import {some} from './export_lib'
 import * as JQuery from "jquery";
 const $ = JQuery.default;
 
-// Определяем модель
-let user:Object = {}
-
-// Определяем конструктор
-user['init'] = function init(){
-  let user_list = $('#user_list')
-  let row = `<tr>
-                <td>${id}</td>
-                <td>${name}</td>
-                <td>${email}</td>
-                <td>${age}</td>
-            </tr>`
-  user_list.append(row)
-}
-
-// Определеяем массив
-user['data'] = [{
-  'id':1,
-  'name':'Kevin',
-  'email':'shindel@bk.ru',
-  'age':33
-}]
-
-// Определяем добавление пользователя
-
-// Запускаем инициализацию
-user['init']()
+	$('#plus').on('click', operation);
+    $('#minus').on('click', operation);
+    $('#multiplication').on('click', operation);
+    $('#divine').on('click', operation);
