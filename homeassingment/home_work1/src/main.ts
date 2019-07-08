0dfb7c6b2f81e3eb3d542d2806f563517fa0a57e
KevinShindel <kevin.shindel@yahoo.com>
2019-07-08 17:58:27 +0300

    minimize main.ts

--- homeassingment/home_work1/src/main.ts
+++ homeassingment/home_work1/src/main.ts
@@ -1,8 +1,5 @@
-import {some} from './export_lib'
+import {operation} from './export_lib'
 import * as JQuery from "jquery";
 const $ = JQuery.default;
 
-	$('#plus').on('click', operation);
-    $('#minus').on('click', operation);
-    $('#multiplication').on('click', operation);
-    $('#divine').on('click', operation);
+    $('#buttonBlock').children('button').click(operation)
