893387a63ca4dfd3d3e1e937459ee1e51554cb87
KevinShindel <kevin.shindel@yahoo.com>
2019-07-27 16:46:14 +0300

    + Styllin 7 lesson

--- 7/prj/src/app/deck/deck.component.ts
+++ 7/prj/src/app/deck/deck.component.ts
@@ -5,15 +5,17 @@ import { Card } from '../card.class';
 @Component({
   selector: 'app-deck',
   template: `
+    <div id="desktop">
   <div id="deck">
   <ul> <li *ngFor="let c of deck.cards"> <app-card [card]="c"></app-card> </li> </ul>
   </div>
-  <div id="userCards">
+  <div id="getUserCards">
   <button (click)="doGetCard()">Get card</button>
   <div id="userCards">
      <div *ngFor="let uc of UserCards"> <app-card [card]="uc"></app-card>  </div>
   </div>
   </div>
+    </div>
   `,
   styleUrls: ['./deck.component.css']
 })
@@ -27,7 +29,7 @@ export class DeckComponent implements OnInit {
     this.deck.shuffleDeck(3);
   }
 
-  doGetCard(){
+  doGetCard() {
     const card = this.deck.getCard();
     card.faceUp();
     this.UserCards.push(card);
